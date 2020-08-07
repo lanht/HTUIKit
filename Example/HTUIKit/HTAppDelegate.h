5115e4dfb1b7b30c38a6efb9e9068967dc86975f
lanht <2318203932@qq.com>
2020-08-07 18:05:35 +0800

    Initial commit

---
+++ Example/HTUIKit/HTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HTAppDelegate.h
+//  HTUIKit
+//
+//  Created by lanht on 08/07/2020.
+//  Copyright (c) 2020 lanht. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
